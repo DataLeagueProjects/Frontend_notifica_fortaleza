be8d2f70334a4259110a776dbe235ab2bfe0d54d
DataLeagueProjects <datascienceleagueprojects@gmail.com>
2024-11-09 20:44:18 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { render } from 'preact'
 // main.jsx
 import App from '/src/app.jsx';
 
-// Your application setup/render logic here using the 'App' component
-
 import './index.css'
 
 render(<App />, document.getElementById('app'))
